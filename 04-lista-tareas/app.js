e0d6a1b78022e1cdba70e5e04085c376288c8e79
Diego Pacori <dpacoria@unsa.edu.pe>
2023-05-29 23:02:25 -0500

    Se creo la funcion para agregar tareas y se agrego la funcionalidad en la consola

--- 04-lista-tareas/app.js
+++ 04-lista-tareas/app.js
@@ -1,23 +1,35 @@
 require('colors');
 
 const inquirer = require('inquirer');
-const {inquirerMenu, pausa} = require('./helpers/inquirer');
+const {inquirerMenu, pausa, leerInput} = require('./helpers/inquirer');
 const Tarea = require('./models/tarea');
 const Tareas = require('./models/tareas');
-// const { mostrarMenu, pausa } = require('./helpers/mensajes.js')
+const { mostrarMenu } = require('./helpers/mensajes.js')
 
 const main = async () => {
     console.log('Hola Mundo');
+
     let opt = '';
+    const tareas = new Tareas();
+
     do{
-        // opt = await mostrarMenu();
-        // console.log({opt});
+        opt = await mostrarMenu();
+        console.log({opt});
+
+        switch(opt) {
+            case '1':
+                //Crear Opcion
+                const desc = await leerInput('Descripcion: ');
+                tareas.crearTarea(desc);
+                break;
+
+            case '2':
+                console.log(tareas._listado);
+                break;
+            
+        }
         // if(opt !== '0') await pausa();
         // opt = await inquirerMenu();
-        const tareas = new Tareas();
-        const tarea = new Tarea('Comprar comida');
-        tareas._listado[tarea.id] = tarea;
-        console.log(tareas);
 
         await pausa();
 
