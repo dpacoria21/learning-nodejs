7c204910922dd0bd4ae319b5cc9bafb8126446e9
Diego Pacori <dpacoria@unsa.edu.pe>
2023-06-15 18:46:36 -0500

    Se utilizo el metodo http que viene incluido en node por defecto para conocer como era antes y el porqué muchos prefieren EXPRESS

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -2,7 +2,15 @@ const http = require('http');
 
 http.createServer((req, res) => {
 
-    res.write('Hola Mundo');
+    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
+    res.writeHead(200, {'Content-Type': 'application/csv'});
+
+    res.write('id, nombre\n');
+    res.write('1, Fernando\n');
+    res.write('2, Jose\n');
+    res.write('3, Sebastian\n');
+    res.write('4, Jerico\n');
+    
     res.end();
 
 })
