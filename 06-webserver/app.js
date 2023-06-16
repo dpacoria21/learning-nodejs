09b11c7b43f3cb1935dca6f50a82b4e57902aa49
Diego Pacori <dpacoria@unsa.edu.pe>
2023-06-15 19:15:59 -0500

    Se creo un servidor para servir una pagina html con css

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,18 +1,22 @@
 const express = require('express');
-
 const app = express();
 const port = 8080;
 
-app.get('/', function(req, res) {
-    res.send('Home Page')
-});
+
+// servir contenido estatico
+app.use( express.static('public') );
+
+
+// app.get('/', function(req, res) {
+//     res.send('Home Page')
+// });
 
 app.get('/hola-mundo', (req, res) => {
     res.send('Hola mundo desde su ruta');
 });
 
 app.get('*', function(req, res) {
-    res.send('Page not found');
+    res.sendFile(__dirname+'/public/404.html');
 })
 
 app.listen(port, () => {
