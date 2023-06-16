b7a7681ab951d3a4a47a17f37aded4662925bceb
Diego Pacori <dpacoria@unsa.edu.pe>
2023-06-15 19:06:55 -0500

    Cree mi primer servidor con express

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,19 +1,20 @@
-const http = require('http');
+const express = require('express');
 
-http.createServer((req, res) => {
+const app = express();
+const port = 8080;
 
-    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
-    res.writeHead(200, {'Content-Type': 'application/csv'});
+app.get('/', function(req, res) {
+    res.send('Home Page')
+});
 
-    res.write('id, nombre\n');
-    res.write('1, Fernando\n');
-    res.write('2, Jose\n');
-    res.write('3, Sebastian\n');
-    res.write('4, Jerico\n');
-    
-    res.end();
+app.get('/hola-mundo', (req, res) => {
+    res.send('Hola mundo desde su ruta');
+});
 
+app.get('*', function(req, res) {
+    res.send('Page not found');
 })
-.listen( 8080 );
 
-console.log('Escuchando en el puerto', 8080);
+app.listen(port, () => {
+    console.log(`Corriendo el servidor en el puerto: ${port}`);
+});
