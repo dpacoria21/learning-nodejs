ba73ab0d05c84841bbfa310335155f691c5358c7
Diego Pacori <dpacoria@unsa.edu.pe>
2023-06-15 19:33:25 -0500

    Se sirvio una pagin web y se controlo las rutas

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -11,9 +11,13 @@ app.use( express.static('public') );
 //     res.send('Home Page')
 // });
 
-app.get('/hola-mundo', (req, res) => {
-    res.send('Hola mundo desde su ruta');
+app.get('/generic', (req, res) => {
+    res.sendFile(__dirname+'/public/generic.html');
 });
+
+app.get('/elements', (req, res) => {
+    res.sendFile(__dirname+'/public/elements.html');
+})
 
 app.get('*', function(req, res) {
     res.sendFile(__dirname+'/public/404.html');
